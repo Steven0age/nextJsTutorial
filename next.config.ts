b757d6b3a61e9b484575cc043b22e139ff19b978
Stephan Haak <info@stephan-haak.com>
2026-06-16 08:34:56 +0200

    add deploy.yml for gh actions

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* config options here 
+  How to deplay on Github can be read here: https://github.com/orgs/community/discussions/191018 */
+  output: "export",
   images: {
+    unoptimized: true,
     remotePatterns: [{ hostname: "stephan-haak.com" }],
   },
+  basePath: "/nextJsTutorial",
+  assetPrefix: "/nextJsTutorial/",
 };
 
 export default nextConfig;
