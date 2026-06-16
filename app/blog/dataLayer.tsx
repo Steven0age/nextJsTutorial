155a2e3a4d48a05858dc72be9996afb851103446
Stephan Haak <info@stephan-haak.com>
2026-06-16 08:50:31 +0200

    minor TS fix

--- app/blog/dataLayer.tsx
+++ app/blog/dataLayer.tsx
@@ -15,12 +15,10 @@ export default function DataLayer({ posts }: any) {
       {showContent &&
         posts.map((post: any) => {
           return (
-            <>
-              <div className="pt-2 pb-10 w-200 border-2">
-                <h2 className="text-xl  text-amber-100">{post.title}</h2>
-                <p>{post.body}</p>
-              </div>
-            </>
+            <div key={post.id} className="pt-2 pb-10 w-200 border-2">
+              <h2 className="text-xl  text-amber-100">{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
           );
         })}
     </>
